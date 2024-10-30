2ac00f1f7e56219b1334fad7b948f470857144a9
fidan86 <ayka-nurka-2015@mail.ru>
2024-10-30 14:59:13 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+// 1) fordan isdifade ederek yuxaridaki hisseni logda cixaran kod yazin
+// *
+// **
+// ***
+// ****
+// *****
+
+// for (let i = 1; i <= 5; i++) {
+//   let star = " ";
+//   for (let j = 1; j <= i; j++) {
+//     star += "*";
+//   }
+//   console.log(star);
+  
+// }
+
+
+
+
+// 2) fordan isdifade ederek yuxaridaki neticeni alan kod yazin
+
+// *****
+// ****
+// ***
+// **
+// * 
+
+
+// for (let i =5; i>=1; i--){
+//     let star =" "
+//     for(let j =1; j<=i; j++){
+//         star+="*"
+//     }
+//     console.log(star);
+//   }
+
+// 3) istifadeciden ad soyad ve yas kimi deyerler alin 
+// ve sonda isdifadecinin dogum ilini qeyd edib loga veren kod yazin
+// meselen "Ali Aliyev siz 1999 cu ilde anadan olmusuz"
+
+// let name = prompt("ad daxil edin");
+// let soyad = prompt("soyad daxil edin");
+// let yas =prompt("yas daxil edin");
+// let nowYear = 2024;
+
+// let birthYear= nowYear - yas;
+// console.log(name, soyad, "siz", birthYear,"ci", "ilde anadan olmusunuz");
+
+// 4)100 e kimi reqemlerden fordan isdifade ederek 5 e bolunenleri 
+// logda cixaran kod yazin
+
+// for ( let i=0; i<100; i++){
+//        if (i%5==0){
+//        console.log(i);
+//     }
+// }
+
+
+// 5) 2 deyisen yaradin username ve password adinda eger isdifadeci
+// prompda her ikisini duzgun daxil ederse yeni username ile password dogru 
+// olarsa logda onu salamlayan kod cixsin burda prompt ve logical operatorlardan
+// isdifade edeceksiz 
+ 
+// let username = prompt("Username daxil edin")
+// let password = prompt("password daxil edin")
+// let a = "Ayxan";
+// let b = "Ayxan123"
+// if (username==a&&password==b){
+//     console.log("hormetli istifadeci xos gelmisiniz");
+    
+// }else{
+//     console.log("zehmet olmasa adinizi ve ya parolunuzu duzgun daxil edin");
+    
+// }
+
